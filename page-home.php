b51208ddf8d8e9390b6038701b58e00be794a29f
Your Name <you@example.com>
2021-07-27 22:08:57 -0300

    adcionado_campos_personalizaveis

--- page-home.php
+++ page-home.php
@@ -55,13 +55,8 @@ get_header(); ?>
 			<div class="container">
 				<h2 class="subtitulo">Portfólio</h2>
 				<?php include(TEMPLATEPATH . "/inc/cliente-portfolio.php" ) ?>
-				<div class="call">
-					<p><?php the_field('chamada_portfolio'); ?></p>
-					<a href="bicke/portfolio" class="btn">Portfólio</a>
-				</div>
 			</div>
 	</section>
-
-	<?php include(TEMPLATEPATH . "/inc/qualidade.php" ) ?>
+<?php include(TEMPLATEPATH . "/inc/qualidade.php" ) ?>
 
 <?php get_footer(); ?>		
